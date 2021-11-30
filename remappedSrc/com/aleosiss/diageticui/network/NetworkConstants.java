87b65d745c8b360fd16fdb50f821dd964b7657e0
aleosiss <askirasa@gmail.com>
2021-11-30 18:16:29 -0500

    finish update

--- remappedSrc/com/aleosiss/diageticui/network/NetworkConstants.java
+++
@@ -1,9 +0,0 @@
-package com.aleosiss.diageticui.network;
-
-import com.aleosiss.diageticui.DiageticUI;
-import net.minecraft.util.Identifier;
-
-public class NetworkConstants {
-    public static final Identifier DIAGETIC_INVENTORY_REQUEST_PACKET = new Identifier(DiageticUI.MOD_ID, "inventory_request_packet");
-    public static final Identifier DIAGETIC_INVENTORY_RESPONSE_PACKET = new Identifier(DiageticUI.MOD_ID, "inventory_response_packet");
-}
--- remappedSrc/com/aleosiss/diageticui/network/NetworkHandlers.java
+++
@@ -1,4 +0,0 @@
-package com.aleosiss.diageticui.network;
-
-public class NetworkHandlers {
-}
